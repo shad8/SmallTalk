c0562dbaa0b4ad2fd7547812cd7c3a598feb48de
shad8 <uholodniak@gmail.com>
2015-11-27 22:11:09 +0100

    Method for receive message

--- SmallTakl/Connector.cs
+++ SmallTakl/Connector.cs
@@ -11,13 +11,17 @@ namespace SmallTakl
   class Connector
   {
     Socket socket;
-    EndPoint epLocal, elRemote;
+    EndPoint epLocal, epRemote;
+
+    string receiveMessage = "";
 
     public Connector()
     {
       socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
       socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
     }
+
+    public string ReceiveMessage { get { return receiveMessage;  } }
 
     public string GetLocalIP()
     {
@@ -28,6 +32,20 @@ namespace SmallTakl
           return ip.ToString();
       }
       return "127.0.0.1";
+    }
+
+    private void MessageCallBack(IAsyncResult aResult)
+    {
+      int size = socket.EndReceiveFrom(aResult, ref epRemote);
+      if (size > 0)
+      {
+        byte[] receiveData = new byte[1462];
+        receiveData = (byte[])aResult.AsyncState;
+        receiveMessage = new ASCIIEncoding().GetString(receiveData);
+      }
+
+      byte[] buffer = new byte[1500];
+      socket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
     }
   }
 }
