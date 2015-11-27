8de4b21c9dbd1a75fa97cc93c743ff5c590c58f1
shad8 <uholodniak@gmail.com>
2015-11-27 19:50:48 +0100

    Add component for client template

---
+++ SmallTakl/Client.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace SmallTakl
+{
+  partial class Client
+  {
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.client1GroupBox = new System.Windows.Forms.GroupBox();
+      this.client2GroupBox = new System.Windows.Forms.GroupBox();
+      this.IPClient1TextBox = new System.Windows.Forms.TextBox();
+      this.IPClient2TextBox = new System.Windows.Forms.TextBox();
+      this.portClient1TextBox = new System.Windows.Forms.TextBox();
+      this.portClient2TextBox = new System.Windows.Forms.TextBox();
+      this.messageTextBox = new System.Windows.Forms.TextBox();
+      this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+      this.listMessage = new System.Windows.Forms.ListBox();
+      this.sendButton = new System.Windows.Forms.Button();
+      this.startButon = new System.Windows.Forms.Button();
+      this.IPClient1Label = new System.Windows.Forms.Label();
+      this.portClient1Label = new System.Windows.Forms.Label();
+      this.IPClient2Label = new System.Windows.Forms.Label();
+      this.portClient2Label = new System.Windows.Forms.Label();
+      this.client1GroupBox.SuspendLayout();
+      this.client2GroupBox.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // client1GroupBox
+      // 
+      this.client1GroupBox.Controls.Add(this.portClient1Label);
+      this.client1GroupBox.Controls.Add(this.IPClient1Label);
+      this.client1GroupBox.Controls.Add(this.portClient1TextBox);
+      this.client1GroupBox.Controls.Add(this.IPClient1TextBox);
+      this.client1GroupBox.Location = new System.Drawing.Point(13, 26);
+      this.client1GroupBox.Name = "client1GroupBox";
+      this.client1GroupBox.Size = new System.Drawing.Size(230, 100);
+      this.client1GroupBox.TabIndex = 0;
+      this.client1GroupBox.TabStop = false;
+      this.client1GroupBox.Text = "Client1";
+      // 
+      // client2GroupBox
+      // 
+      this.client2GroupBox.Controls.Add(this.IPClient2Label);
+      this.client2GroupBox.Controls.Add(this.portClient2Label);
+      this.client2GroupBox.Controls.Add(this.portClient2TextBox);
+      this.client2GroupBox.Controls.Add(this.IPClient2TextBox);
+      this.client2GroupBox.Location = new System.Drawing.Point(249, 26);
+      this.client2GroupBox.Name = "client2GroupBox";
+      this.client2GroupBox.Size = new System.Drawing.Size(230, 100);
+      this.client2GroupBox.TabIndex = 1;
+      this.client2GroupBox.TabStop = false;
+      this.client2GroupBox.Text = "Client2";
+      // 
+      // IPClient1TextBox
+      // 
+      this.IPClient1TextBox.Location = new System.Drawing.Point(88, 29);
+      this.IPClient1TextBox.Name = "IPClient1TextBox";
+      this.IPClient1TextBox.Size = new System.Drawing.Size(136, 20);
+      this.IPClient1TextBox.TabIndex = 0;
+      // 
+      // IPClient2TextBox
+      // 
+      this.IPClient2TextBox.Location = new System.Drawing.Point(88, 30);
+      this.IPClient2TextBox.Name = "IPClient2TextBox";
+      this.IPClient2TextBox.Size = new System.Drawing.Size(136, 20);
+      this.IPClient2TextBox.TabIndex = 0;
+      // 
+      // portClient1TextBox
+      // 
+      this.portClient1TextBox.Location = new System.Drawing.Point(88, 56);
+      this.portClient1TextBox.Name = "portClient1TextBox";
+      this.portClient1TextBox.Size = new System.Drawing.Size(136, 20);
+      this.portClient1TextBox.TabIndex = 1;
+      // 
+      // portClient2TextBox
+      // 
+      this.portClient2TextBox.Location = new System.Drawing.Point(88, 56);
+      this.portClient2TextBox.Name = "portClient2TextBox";
+      this.portClient2TextBox.Size = new System.Drawing.Size(136, 20);
+      this.portClient2TextBox.TabIndex = 1;
+      // 
+      // messageTextBox
+      // 
+      this.messageTextBox.Location = new System.Drawing.Point(13, 343);
+      this.messageTextBox.Name = "messageTextBox";
+      this.messageTextBox.Size = new System.Drawing.Size(449, 20);
+      this.messageTextBox.TabIndex = 2;
+      // 
+      // contextMenuStrip1
+      // 
+      this.contextMenuStrip1.Name = "contextMenuStrip1";
+      this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+      // 
+      // listMessage
+      // 
+      this.listMessage.FormattingEnabled = true;
+      this.listMessage.Location = new System.Drawing.Point(13, 138);
+      this.listMessage.Name = "listMessage";
+      this.listMessage.Size = new System.Drawing.Size(532, 186);
+      this.listMessage.TabIndex = 4;
+      // 
+      // sendButton
+      // 
+      this.sendButton.Location = new System.Drawing.Point(470, 340);
+      this.sendButton.Name = "sendButton";
+      this.sendButton.Size = new System.Drawing.Size(75, 23);
+      this.sendButton.TabIndex = 5;
+      this.sendButton.Text = "Send";
+      this.sendButton.UseVisualStyleBackColor = true;
+      // 
+      // startButon
+      // 
+      this.startButon.Location = new System.Drawing.Point(485, 32);
+      this.startButon.Name = "startButon";
+      this.startButon.Size = new System.Drawing.Size(60, 94);
+      this.startButon.TabIndex = 6;
+      this.startButon.Text = "START";
+      this.startButon.UseVisualStyleBackColor = true;
+      // 
+      // IPClient1Label
+      // 
+      this.IPClient1Label.AutoSize = true;
+      this.IPClient1Label.Location = new System.Drawing.Point(7, 36);
+      this.IPClient1Label.Name = "IPClient1Label";
+      this.IPClient1Label.Size = new System.Drawing.Size(17, 13);
+      this.IPClient1Label.TabIndex = 2;
+      this.IPClient1Label.Text = "IP";
+      // 
+      // portClient1Label
+      // 
+      this.portClient1Label.AutoSize = true;
+      this.portClient1Label.Location = new System.Drawing.Point(7, 63);
+      this.portClient1Label.Name = "portClient1Label";
+      this.portClient1Label.Size = new System.Drawing.Size(26, 13);
+      this.portClient1Label.TabIndex = 3;
+      this.portClient1Label.Text = "Port";
+      // 
+      // IPClient2Label
+      // 
+      this.IPClient2Label.AutoSize = true;
+      this.IPClient2Label.Location = new System.Drawing.Point(6, 36);
+      this.IPClient2Label.Name = "IPClient2Label";
+      this.IPClient2Label.Size = new System.Drawing.Size(17, 13);
+      this.IPClient2Label.TabIndex = 4;
+      this.IPClient2Label.Text = "IP";
+      // 
+      // portClient2Label
+      // 
+      this.portClient2Label.AutoSize = true;
+      this.portClient2Label.Location = new System.Drawing.Point(6, 63);
+      this.portClient2Label.Name = "portClient2Label";
+      this.portClient2Label.Size = new System.Drawing.Size(26, 13);
+      this.portClient2Label.TabIndex = 5;
+      this.portClient2Label.Text = "Port";
+      // 
+      // Client
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.startButon);
+      this.Controls.Add(this.sendButton);
+      this.Controls.Add(this.listMessage);
+      this.Controls.Add(this.messageTextBox);
+      this.Controls.Add(this.client2GroupBox);
+      this.Controls.Add(this.client1GroupBox);
+      this.Name = "Client";
+      this.Size = new System.Drawing.Size(563, 385);
+      this.client1GroupBox.ResumeLayout(false);
+      this.client1GroupBox.PerformLayout();
+      this.client2GroupBox.ResumeLayout(false);
+      this.client2GroupBox.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.GroupBox client1GroupBox;
+    private System.Windows.Forms.Label portClient1Label;
+    private System.Windows.Forms.Label IPClient1Label;
+    private System.Windows.Forms.TextBox portClient1TextBox;
+    private System.Windows.Forms.TextBox IPClient1TextBox;
+    private System.Windows.Forms.GroupBox client2GroupBox;
+    private System.Windows.Forms.Label IPClient2Label;
+    private System.Windows.Forms.Label portClient2Label;
+    private System.Windows.Forms.TextBox portClient2TextBox;
+    private System.Windows.Forms.TextBox IPClient2TextBox;
+    private System.Windows.Forms.TextBox messageTextBox;
+    private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    private System.Windows.Forms.ListBox listMessage;
+    private System.Windows.Forms.Button sendButton;
+    private System.Windows.Forms.Button startButon;
+  }
+}
